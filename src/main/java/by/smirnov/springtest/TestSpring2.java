575b5e7db4363761e1fc4966a3aa18012368abb8
AntonioBuenos <104083531+AntonioBuenos@users.noreply.github.com>
2022-08-22 10:42:24 +0300

    Test @Autowired

--- src/main/java/by/smirnov/springtest/TestSpring2.java
+++ src/main/java/by/smirnov/springtest/TestSpring2.java
@@ -6,13 +6,19 @@ public class TestSpring2 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext2.xml");
 
-        Music music = context.getBean("musicBean", Music.class);
+/*        Music music = context.getBean("musicBean", Music.class);
 
         MusicPlayer musicPlayer = new MusicPlayer(music);
         musicPlayer.playMusic();
         Music music1 = context.getBean("classicalMusic", Music.class);
         MusicPlayer classicalPlayer = new MusicPlayer(music1);
         classicalPlayer.playMusic();
-        context.close();
+        context.close();*/
+
+/*        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        musicPlayer.playMusic();*/
+
+        Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);
     }
 }
