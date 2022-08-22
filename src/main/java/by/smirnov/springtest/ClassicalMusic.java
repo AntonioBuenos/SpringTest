af5bdeb558f52f77929efc62f2495db46a635b61
AntonioBuenos <104083531+AntonioBuenos@users.noreply.github.com>
2022-08-22 12:31:47 +0300

    Test @Value, @Scope, @PostConstruct, @PreDestroy

--- src/main/java/by/smirnov/springtest/ClassicalMusic.java
+++ src/main/java/by/smirnov/springtest/ClassicalMusic.java
@@ -1,9 +1,24 @@
 package by.smirnov.springtest;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
+@Scope("singleton")
 public class ClassicalMusic implements Music{
+
+    @PostConstruct
+    public void doMyInit(){
+        System.out.println("Doin' my initialization");
+    }
+
+    @PreDestroy
+    public void doMyDestroy(){
+        System.out.println("Doin' my destruction");
+    }
 
     @Override
     public String getSong() {
