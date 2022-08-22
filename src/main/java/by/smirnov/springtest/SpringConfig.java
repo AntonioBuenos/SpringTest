a532f2374103f841d98f76b37c93da396716532f
AntonioBuenos <104083531+AntonioBuenos@users.noreply.github.com>
2022-08-22 12:40:40 +0300

    Test Java Configuration

---
+++ src/main/java/by/smirnov/springtest/SpringConfig.java
@@ -0,0 +1,12 @@
+package by.smirnov.springtest;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ComponentScan("by.smirnov.springtest")
+@PropertySource("classpath:musicPlayer.properties")
+public class SpringConfig {
+}
